fa156f78b75a48519d5e42b7bf626d60b7a182c9
Jhê Cardoso <jeane.amarela@gmail.com>
2018-08-27 19:52:18 -0300

    Add files via upload

---
+++ Exercicios/src/br/ufpb/estrutura/OperacoesFila.java
@@ -0,0 +1,9 @@
+package br.ufpb.estrutura;
+
+public interface OperacoesFila {
+    public boolean filaVazia();
+    public void inserir(int valor);
+    public void remover();
+    public void imprimir();
+    public void tamanhoFila();
+}
---
+++ Exercicios/src/br/ufpb/estrutura/Teste.java
@@ -0,0 +1,15 @@
+package br.ufpb.estrutura;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        FilaEncadeada f = new FilaEncadeada();
+
+        f.imprimir();
+        f.inserir(4);
+        f.imprimir();
+
+
+
+    }
+}
